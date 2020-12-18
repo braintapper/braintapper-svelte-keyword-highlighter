464434e19a6ea1f71953d622497d2c5c6469649f
Steven Ng <steven.ng@braintapper.com>
2020-12-18 18:08:08 -0500

    Initial

---
+++ src/KeywordHighlighter.svelte
@@ -0,0 +1,18 @@
+<script>
+  export let keywords = [];
+  export let text = "";
+  const highlight = (txt, tokens) => {
+    let output = `${txt}`
+    tokens.forEach((token) => {
+      output = output.replace(new RegExp(`(${token})`,"gi"), "<span class=\"keyword-highlight\">$1</span>");
+     });
+    return output
+  }
+  $: highlightedText = highlight(text,keywords);
+</script>
+{@html highlightedText}
+<style>
+  :global(span.keyword-highlight) {
+    background: var(--lighter-yellow);
+    font-weight: inherit; }
+</style>
